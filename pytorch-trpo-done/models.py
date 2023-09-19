b212dc2308d4e538b035fb9d2c929418a26562d7
xsy786912649 <786912649@qq.com>
2023-09-18 20:11:17 -0400

    update

--- pytorch-trpo-done/models.py
+++ pytorch-trpo-done/models.py
@@ -10,8 +10,8 @@ class Policy(nn.Module):
         self.affine2 = nn.Linear(64, 64)
 
         self.action_mean = nn.Linear(64, num_outputs)
-        self.action_mean.weight.data.mul_(0.1)
-        self.action_mean.bias.data.mul_(0.0)
+        #self.action_mean.weight.data.mul_(1.0)
+        #self.action_mean.bias.data.mul_(0.0)
 
         self.action_log_std = nn.Parameter(torch.zeros(1, num_outputs))
 
@@ -36,8 +36,8 @@ class Value(nn.Module):
         self.affine1 = nn.Linear(num_inputs, 64)
         self.affine2 = nn.Linear(64, 64)
         self.value_head = nn.Linear(64, 1)
-        self.value_head.weight.data.mul_(0.1)
-        self.value_head.bias.data.mul_(0.0)
+        #self.value_head.weight.data.mul_(0.1)
+        #self.value_head.bias.data.mul_(0.0)
 
     def forward(self, x):
         x = torch.tanh(self.affine1(x))
