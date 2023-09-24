ac9316a759b33be255026f719bf77f5940ec1f7c
xsy786912649 <786912649@qq.com>
2023-09-24 19:23:08 -0400

    udpate

---
+++ my_method/main.py
@@ -0,0 +1,34 @@
+import argparse
+from itertools import count
+import torch
+
+torch.utils.backcompat.broadcast_warning.enabled = True
+torch.utils.backcompat.keepdim_warning.enabled = True
+torch.set_default_tensor_type('torch.DoubleTensor')
+
+parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
+parser.add_argument('--gamma', type=float, default=0.995, metavar='G',
+                    help='discount factor (default: 0.995)')
+parser.add_argument('--env-name', default="Reacher-v4", metavar='G',
+                    help='name of the environment to run')
+parser.add_argument('--tau', type=float, default=0.97, metavar='G',
+                    help='gae (default: 0.97)')
+parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
+                    help='l2 regularization regression (default: 1e-3)')
+parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
+                    help='max kl value (default: 1e-2)')
+parser.add_argument('--damping', type=float, default=0e-1, metavar='G',
+                    help='damping (default: 0e-1)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 1)')
+parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
+                    help='batch-size (default: 15000)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=1, metavar='N',
+                    help='interval between training status logs (default: 1)')
+args = parser.parse_args()
+
+
+
+
--- my_method/update.py
+++ my_method/update.py
@@ -28,8 +28,8 @@ parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
                     help='l2 regularization regression (default: 1e-3)')
 parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
                     help='max kl value (default: 1e-2)')
-parser.add_argument('--damping', type=float, default=1e-1, metavar='G',
-                    help='damping (default: 1e-1)')
+parser.add_argument('--damping', type=float, default=0e-1, metavar='G',
+                    help='damping (default: 0e-1)')
 parser.add_argument('--seed', type=int, default=543, metavar='N',
                     help='random seed (default: 1)')
 parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
@@ -37,15 +37,20 @@ parser.add_argument('--batch-size', type=int, default=15000, metavar='N',
 parser.add_argument('--render', action='store_true',
                     help='render the environment')
 parser.add_argument('--log-interval', type=int, default=1, metavar='N',
-                    help='interval between training status logs (default: 10)')
+                    help='interval between training status logs (default: 1)')
 args = parser.parse_args()
 
-env = gym.make(args.env_name)
+def init_env(env_name):
+    env = gym.make(env_name)
+    num_inputs = env.observation_space.shape[0]
+    num_actions = env.action_space.shape[0]
+    return env,num_inputs,num_actions
 
-num_inputs = env.observation_space.shape[0]
-num_actions = env.action_space.shape[0]
+def only_init_env(env_name):
+    env = gym.make(env_name)
+    return env
 
-#env.seed(args.seed)
+env,num_inputs,num_actions=init_env(args.env_name)
 torch.manual_seed(args.seed)
 
 policy_net = Policy(num_inputs, num_actions)
@@ -58,51 +63,58 @@ def select_action(state):
     return action
 
 def update_params(batch):
+
     rewards = torch.Tensor(batch.reward)
     masks = torch.Tensor(batch.mask)
     actions = torch.Tensor(np.concatenate(batch.action, 0))
     states = torch.Tensor(batch.state)
-    values = value_net(Variable(states))
+    
 
-    returns = torch.Tensor(actions.size(0),1)
-    deltas = torch.Tensor(actions.size(0),1)
-    advantages = torch.Tensor(actions.size(0),1)
+    def update_advantage_function():
 
-    prev_return = 0
-    prev_value = 0
-    prev_advantage = 0
-    for i in reversed(range(rewards.size(0))):
-        returns[i] = rewards[i] + args.gamma * prev_return * masks[i]
-        deltas[i] = rewards[i] + args.gamma * prev_value * masks[i] - values.data[i]
-        advantages[i] = deltas[i] + args.gamma * args.tau * prev_advantage * masks[i]
+        values = value_net(Variable(states))
+        returns = torch.Tensor(actions.size(0),1)
+        deltas = torch.Tensor(actions.size(0),1)
+        advantages = torch.Tensor(actions.size(0),1)
 
-        prev_return = returns[i, 0]
-        prev_value = values.data[i, 0]
-        prev_advantage = advantages[i, 0]
+        prev_return = 0
+        prev_value = 0
+        prev_advantage = 0
+        for i in reversed(range(rewards.size(0))):
+            returns[i] = rewards[i] + args.gamma * prev_return * masks[i]
+            deltas[i] = rewards[i] + args.gamma * prev_value * masks[i] - values.data[i]
+            advantages[i] = deltas[i] + args.gamma * args.tau * prev_advantage * masks[i]
 
-    targets = Variable(returns)
+            prev_return = returns[i, 0]
+            prev_value = values.data[i, 0]
+            prev_advantage = advantages[i, 0]
 
-    # Original code uses the same LBFGS to optimize the value loss
-    def get_value_loss(flat_params):
+        targets = Variable(returns)
+
+        # Original code uses the same LBFGS to optimize the value loss
+        def get_value_loss(flat_params):
+            set_flat_params_to(value_net, torch.Tensor(flat_params))
+            for param in value_net.parameters():
+                if param.grad is not None:
+                    param.grad.data.fill_(0)
+
+            values_ = value_net(Variable(states))
+            value_loss = (values_ - targets).pow(2).mean()
+
+            # weight decay
+            for param in value_net.parameters():
+                value_loss += param.pow(2).sum() * args.l2_reg
+            value_loss.backward()
+            return (value_loss.data.double().numpy(), get_flat_grad_from(value_net).data.double().numpy())
+
+        flat_params, _, opt_info = scipy.optimize.fmin_l_bfgs_b(get_value_loss, get_flat_params_from(value_net).double().numpy(), maxiter=25)
         set_flat_params_to(value_net, torch.Tensor(flat_params))
-        for param in value_net.parameters():
-            if param.grad is not None:
-                param.grad.data.fill_(0)
 
-        values_ = value_net(Variable(states))
+        return advantages
 
-        value_loss = (values_ - targets).pow(2).mean()
-
-        # weight decay
-        for param in value_net.parameters():
-            value_loss += param.pow(2).sum() * args.l2_reg
-        value_loss.backward()
-        return (value_loss.data.double().numpy(), get_flat_grad_from(value_net).data.double().numpy())
-
-    flat_params, _, opt_info = scipy.optimize.fmin_l_bfgs_b(get_value_loss, get_flat_params_from(value_net).double().numpy(), maxiter=25)
-    set_flat_params_to(value_net, torch.Tensor(flat_params))
-
-    advantages = (advantages - advantages.mean()) / advantages.std()
+    for i in range(1):
+        advantages=update_advantage_function()
+    advantages = (advantages - advantages.mean()) / (advantages.std()*3.0) 
 
     action_means, action_log_stds, action_stds = policy_net(Variable(states))
     fixed_log_prob = normal_log_density(Variable(actions), action_means, action_log_stds, action_stds).data.clone()
@@ -118,7 +130,6 @@ def update_params(batch):
         action_loss = -Variable(advantages) * torch.exp(log_prob - Variable(fixed_log_prob))
         return action_loss.mean()
 
-
     def get_kl():
         mean1, log_std1, std1 = policy_net(Variable(states))
 
@@ -133,45 +144,48 @@ def update_params(batch):
 running_state = ZFilter((num_inputs,), clip=5)
 running_reward = ZFilter((1,), demean=False, clip=10)
 
-for i_episode in count(1):
-    memory = Memory()
+if __name__ == "__main__":
 
-    num_steps = 0
-    reward_batch = 0
-    num_episodes = 0
-    while num_steps < args.batch_size:
-        state = env.reset()[0]
-        state = running_state(state)
+    for i_episode in count(1):
+        memory = Memory()
 
-        reward_sum = 0
-        for t in range(1000): # Don't infinite loop while learning #50 for reacher
-            action = select_action(state)
-            action = action.data[0].numpy()
-            next_state, reward, done, _,_ = env.step(action)
-            reward_sum += reward
+        num_steps = 0
+        reward_batch = 0
+        num_episodes = 0
+        
+        while num_steps <= args.batch_size-1:
+            state = env.reset()[0]
+            state = running_state(state)
 
-            next_state = running_state(next_state)
+            reward_sum = 0
+            for t in range(1000): # Don't infinite loop while learning #50 for reacher
+                action = select_action(state)
+                action = action.data[0].numpy()
+                next_state, reward, done, _,_ = env.step(action)
+                reward_sum += reward
 
-            mask = 1
-            if done:
-                mask = 0
+                next_state = running_state(next_state)
 
-            memory.push(state, np.array([action]), mask, next_state, reward)
+                mask = 1
+                if done:
+                    mask = 0
 
-            if args.render:
-                env.render()
-            if done:
-                break
+                memory.push(state, np.array([action]), mask, next_state, reward)
 
-            state = next_state
-        num_steps += (t-1)
-        num_episodes += 1
-        reward_batch += reward_sum
+                if args.render:
+                    env.render()
+                if done:
+                    break
 
-    reward_batch /= num_episodes
-    batch = memory.sample()
-    update_params(batch)
+                state = next_state
+            num_steps += (t+1)
+            num_episodes += 1
+            reward_batch += reward_sum
 
-    if i_episode % args.log_interval == 0:
-        print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
-            i_episode, reward_sum, reward_batch))
+        reward_batch /= num_episodes
+        batch = memory.sample()
+        update_params(batch)
+
+        if i_episode % args.log_interval == 0:
+            print('Episode {}\tLast reward: {}\tAverage reward {:.2f}'.format(
+                i_episode, reward_sum, reward_batch))
