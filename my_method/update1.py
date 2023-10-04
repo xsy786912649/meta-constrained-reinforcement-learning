23400dd9925ce9777ec208ef9e9683385c307f7b
xsy786912649 <786912649@qq.com>
2023-10-04 14:58:34 -0400

    Update update1.py

--- my_method/update1.py
+++ my_method/update1.py
@@ -27,8 +27,8 @@ parser.add_argument('--tau', type=float, default=0.97, metavar='G',
                     help='gae (default: 0.97)')
 parser.add_argument('--l2-reg', type=float, default=1e-3, metavar='G',
                     help='l2 regularization regression (default: 1e-3)')
-parser.add_argument('--meta-lambda', type=float, default=0.25, metavar='G',
-                    help='meta meta-lambda (default: 0.25)') 
+parser.add_argument('--meta-lambda', type=float, default=0.15, metavar='G',
+                    help='meta meta-lambda (default: 0.15)') 
 parser.add_argument('--max-kl', type=float, default=3e-2, metavar='G',
                     help='max kl value (default: 3e-2)')
 parser.add_argument('--damping', type=float, default=0e-1, metavar='G',
