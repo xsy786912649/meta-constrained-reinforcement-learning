36c0616031d0124ea64e306149a620087fdb4153
xsy786912649 <786912649@qq.com>
2024-01-09 12:26:59 -0500

    Update test.py

--- my_method_new/test.py
+++ my_method_new/test.py
@@ -58,8 +58,7 @@ if __name__ == "__main__":
         print(torch.exp(meta_policy_net.action_log_std)) 
 
         for iteration_number in range(4):
-            #_,accumulated_raward_batch=sample_data_for_task_specific_test(target_v,previous_policy_net,args.batch_size) 
-            
+            _,accumulated_raward_batch=sample_data_for_task_specific_test(target_v,previous_policy_net,args.batch_size)
             batch,batch_extra,accumulated_raward_batch2=sample_data_for_task_specific(target_v,previous_policy_net,args.batch_size)
             print("task_number: ",task_number)
             #print('(adaptation {}) \tAverage reward {:.2f}'.format(iteration_number, accumulated_raward_batch))
